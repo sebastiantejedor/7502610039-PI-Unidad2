40faed0df1b3dc2dab330769f1ce799bb7982a3b
Sebastián Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-08-30 15:42:19 -0500

    feat(ejercicio1): agrego ejercicio1

---
+++ src/Estudiante.java
@@ -0,0 +1,47 @@
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private double notaPromedio;
+
+    public Estudiante(String nombre, int edad, double notaPromedio) {
+        this.setNombre(nombre);
+        this.setEdad(edad);
+        this.setNotaPromedio(notaPromedio);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre != null && !nombre.trim().isEmpty()) {
+            this.nombre = nombre;
+        } else {
+            System.out.println("El nombre no puede estar vacío.");
+        }
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0) {
+            this.edad = edad;
+        } else {
+            System.out.println("La edad no puede ser negativa.");
+        }
+    }
+
+    public double getNotaPromedio() {
+        return notaPromedio;
+    }
+
+    public void setNotaPromedio(double notaPromedio) {
+        if (notaPromedio >= 0 && notaPromedio <= 5) {
+            this.notaPromedio = notaPromedio;
+        } else {
+            System.out.println("La nota debe estar entre 0 y 5.");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+public class Main {
+    public static void main(String[] args) {
 
+        Estudiante estudiante1 = new Estudiante("Sebas", 17, 4.5);
+
+        System.out.println("Nombre: " + estudiante1.getNombre());
+        System.out.println("Edad: " + estudiante1.getEdad());
+        System.out.println("Nota promedio: " + estudiante1.getNotaPromedio());
+
+        estudiante1.setNombre("Sebastián");
+        estudiante1.setEdad(18);
+        estudiante1.setNotaPromedio(4.8);
+
+        System.out.println("\nDatos actualizados:");
+        System.out.println("Nombre: " + estudiante1.getNombre());
+        System.out.println("Edad: " + estudiante1.getEdad());
+        System.out.println("Nota promedio: " + estudiante1.getNotaPromedio());
+
+        estudiante1.setEdad(-5);
+        estudiante1.setNotaPromedio(7);
+    }
 }
